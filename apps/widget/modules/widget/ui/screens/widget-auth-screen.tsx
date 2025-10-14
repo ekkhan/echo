c900d455b430e69c33f17ef7953d9f3e5f7b8945
Ekaterina Khan <ekkhan@ucsc.edu>
2025-10-14 11:31:00 -0700

    widget session

---
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -0,0 +1,126 @@
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+  } from "@workspace/ui/components/form";
+import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
+
+import { WidgetHeader } from "../components/widget-header";
+import { useMutation } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { Doc } from "@workspace/backend/_generated/dataModel";
+
+const formSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().email("enter a valid email"),
+  });
+
+//temporary test id
+const organizationId="123";
+
+export const WidgetAuthScreen = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          name: "",
+          email: "",
+        },
+      });
+
+    const createContactSession = useMutation(api.public.contactSessions.create)
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+      if (!organizationId) {
+        return;
+      }
+      const metaData: Doc<"contactSessions">["metaData"] = {
+        userAgent: navigator.userAgent,
+        language: navigator.language,
+        languages: navigator.languages?.join(","),
+        platform: navigator.platform,
+        vendor: navigator.vendor,
+        screenResolution: `${screen.width}x${screen.height}`,
+        viewportSize: `${window.innerWidth}x${window.innerHeight}`,
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        timezoneOffset: new Date().getTimezoneOffset(),
+        cookieEnabled: navigator.cookieEnabled,
+        referrer: document.referrer || "direct",
+        currentUrl: window.location.href,
+      };
+
+      const contactSessionId = await createContactSession({
+        ...values,
+        organizationId,
+        metaData
+      })
+      console.log({contactSessionId});
+
+    };
+    
+    return (
+        <>
+        <WidgetHeader>
+            <div>
+                <p className="text-3xl">
+                    Hi there! 👋
+                </p>
+                <p className="text-lg">
+                    Lets&apos;s get you started
+                </p>
+            </div>
+        </WidgetHeader>
+        <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-1 flex-col gap-y-4 p-4"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="h-10 bg-background"
+                    placeholder="Name"
+                    type="text"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="h-10 bg-background"
+                    placeholder="Email"
+                    type="email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            disabled={form.formState.isSubmitting}
+            size="lg"
+            type="submit"
+          >Continue</Button>
+        </form>
+      </Form>
+        </>
+    )
+}
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -2,6 +2,7 @@
 
 import { WidgetFooter } from "../components/widget-footer";
 import { WidgetHeader } from "../components/widget-header";
+import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface Props {
     organizationId: string;
@@ -9,21 +10,8 @@ interface Props {
 
 export const WidgetView = ({organizationId}:Props) => {
     return (<main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-        
-        <WidgetHeader>
-            <div>
-                <p className="text-3xl">
-                    Hi there! 👋
-                </p>
-                <p className="text-lg">
-                    How can we help you today?
-                </p>
-            </div>
-        </WidgetHeader>
-       <div className="flex flex-1"> 
-        Widget view: {organizationId}
-         </div>
-        <WidgetFooter/>
+        <WidgetAuthScreen/>
+        {/* <WidgetFooter/> */}
         </main>
     );
 };
