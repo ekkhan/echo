a779603458ab8c17329e3961346468c9a05e8180
Ekaterina Khan <ekkhan@ucsc.edu>
2025-10-19 00:26:41 -0700

    widget inbox

--- apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
@@ -8,6 +8,7 @@ import { contactSessionIdAtomFamily, conversationIdAtom, errorMessageAtom, organ
 import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { useState } from "react";
+import { WidgetFooter } from "../components/widget-footer";
 
 export const WidgetSelectionScreen = () => {
     const setErrorMessage = useSetAtom(errorMessageAtom);
@@ -71,6 +72,7 @@ export const WidgetSelectionScreen = () => {
                 <ChevronRightIcon />
             </Button>
         </div>
+        <WidgetFooter/>
         </>
     );
 }
