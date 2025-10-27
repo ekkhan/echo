e8fcaf0e28f75b87cbc54bf9f6f4067f7b824314
Ekaterina Khan <ekkhan@ucsc.edu>
2025-10-27 00:29:45 -0700

    subscriptions

---
+++ apps/web/modules/billing/ui/views/billing-view.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { PricingTable } from "../components/pricing-table";
+
+export const BillingView = () => {
+    return (
+        <div className="flex min-h-screen flex-col bg-muted p-8">
+            <div className="mx-auto w-full max-w-screen-md">
+              <div className="space-y-2">
+                <h1 className="text-2xl md:text-4xl">Plans & Billing</h1>
+                <p className="text-muted-foreground">
+                  Choose the plan that's right for you and your team.
+                </p>
+              </div>
+
+              <div className="mt-8">
+                <PricingTable />
+              </div>
+            </div>
+        </div>
+    );
+};
